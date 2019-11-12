1e30d3e1796043abf96c5efad3dfd20749faeebf
Ray Meredith <ray.meredith@gmail.com>
2019-11-11 19:07:02 -0600

    list of word DIVs is now draggable/sortable

--- src/routes/Sentence.svelte
+++ src/routes/Sentence.svelte
@@ -1,4 +1,27 @@
 <script>
+    import { onMount } from 'svelte';
+    import Draggable from '@shopify/draggable';
+
+    onMount(() => {
+        const sortable = new Draggable.Sortable(
+            document.querySelector('#wordContainer'), {
+                draggable: 'div',
+            }
+        );
+        sortable.on('sortable:start', () => {
+            console.log('sortable:start')
+        })
+        sortable.on('sortable:sort', () => {
+            console.log('sortable:sort')
+        })
+        sortable.on('sortable:sorted', () => {
+            console.log('sortable:sorted')
+        })
+        sortable.on('sortable:stop', () => {
+            console.log('sortable:stop')
+        })
+    });
+
     import {
         sentence,
         currentResponse
@@ -12,26 +35,49 @@
 </script>
 
 <style>
-    #wordContainer {
-        /* position: relative; */
+    /* #wordContainer {
+        position: relative;
         display: flex;
         flex-wrap: wrap;
         min-width: 100px;
         max-width: 500px;
     }
+
     #wordContainer .item {
         flex-basis: auto;
         background-color: lightblue;
         margin: 0px 0px 12px 0px;
         padding: 0px 10px 0px 10px;
     }
+     */
+
+    #wordContainer {
+        min-width: 100px;
+        max-width: 500px;
+    }
+
+    #wordContainer>div {
+		display: inline-block;
+		margin: 0px;
+		padding: 6px 0px 6px 0px;
+		color: red;
+		font-size: 1em;
+        background-color: chartreuse;
+        cursor: pointer;
+		/* text-shadow: 0 0 20px rgba(0,0,0,0.3); */
+	}
+
+	:global(.draggable-source--is-dragging) {
+		visibility: hidden;
+        cursor: grabbing;
+	}
 </style>
 
 <p>{$sentence}</p>
 <div id="wordContainer">
     {#each theWords as aWord, i}
         {#if i < (theWords.length - 1)}
-            <div class="item">{aWord}</div>&nbsp;-&nbsp;
+            <div class="item">{aWord}&nbsp;-&nbsp;</div>
         {:else}
             <div class="item">{aWord}</div>
         {/if}
